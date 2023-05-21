0357a50398d9e24286b91bd77d9fd1a7c5bbb69e
patrickstracey <pstracey17@gmail.com>
2023-05-21 16:00:41 -0700

    init `user` module and controller

---
+++ backend/src/user/user.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { MongoModule } from '../database/mongo';
+
+@Module({
+  imports: [MongoModule],
+  controllers: [UserController],
+  providers: [UserService],
+  exports: [UserService],
+})
+export class UserModule {}
