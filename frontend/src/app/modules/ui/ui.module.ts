11e34bdadea24fb4869ddccd72c4807327fab0d7
patrickstracey <pstracey17@gmail.com>
2023-05-22 22:33:01 -0700

    phone pipe

--- frontend/src/app/modules/ui/ui.module.ts
+++ frontend/src/app/modules/ui/ui.module.ts
@@ -1,16 +1,25 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {CardComponent} from "./card/card.component";
-import {PageComponent} from "./page/page.component";
-import {LoaderComponent} from "./loader/loader.component";
-import {NoResultsComponent} from "./no-results/no-results.component";
-
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardComponent } from './card/card.component';
+import { PageComponent } from './page/page.component';
+import { LoaderComponent } from './loader/loader.component';
+import { NoResultsComponent } from './no-results/no-results.component';
+import { PipesModule } from '../../utility/pipes/pipes.module';
 
 @NgModule({
-  declarations: [CardComponent, PageComponent, LoaderComponent, NoResultsComponent],
-  imports: [
-    CommonModule
+  declarations: [
+    CardComponent,
+    PageComponent,
+    LoaderComponent,
+    NoResultsComponent,
   ],
-  exports: [CardComponent, PageComponent, LoaderComponent, NoResultsComponent]
+  imports: [CommonModule, PipesModule],
+  exports: [
+    CardComponent,
+    PageComponent,
+    LoaderComponent,
+    NoResultsComponent,
+    PipesModule,
+  ],
 })
-export class UiModule { }
+export class UiModule {}
