78175b2a97d6a70b6454b02a6172b1499d760f24
patrickstracey <pstracey17@gmail.com>
2023-05-22 22:33:55 -0700

    interceptor

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationModule } from './modules/navigation/navigation.module';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { UiModule } from './modules/ui/ui.module';
 import { AuthPagesModule } from './pages/auth-pages/auth-pages.module';
+import { AuthInterceptor } from './utility/interceptors';
 
 @NgModule({
   declarations: [AppComponent, HomePageComponent],
@@ -18,7 +19,9 @@ import { AuthPagesModule } from './pages/auth-pages/auth-pages.module';
     UiModule,
     AuthPagesModule,
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
