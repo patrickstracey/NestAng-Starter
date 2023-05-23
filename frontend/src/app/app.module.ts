b374a2d7b696c234649b76c946638e0deb0d25a0
patrickstracey <pstracey17@gmail.com>
2023-05-22 21:18:52 -0700

    module imports

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,25 +1,24 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {HttpClientModule} from "@angular/common/http";
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {NavigationModule} from "./modules/navigation/navigation.module";
-import {HomePageComponent} from './pages/home-page/home-page.component';
-import {UiModule} from "./modules/ui/ui.module";
-
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavigationModule } from './modules/navigation/navigation.module';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { UiModule } from './modules/ui/ui.module';
+import { AuthPagesModule } from './pages/auth-pages/auth-pages.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomePageComponent
-  ],
+  declarations: [AppComponent, HomePageComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    NavigationModule, UiModule
+    NavigationModule,
+    UiModule,
+    AuthPagesModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- frontend/src/app/pages/auth-pages/auth-pages.module.ts
+++ frontend/src/app/pages/auth-pages/auth-pages.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AuthRoutingModule } from './auth-routing.module';
-import { UiModule } from '../../modules/ui';
+import { UiModule } from '../../modules/ui/ui.module';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { SignupPageComponent } from './signup-page/signup-page.component';
 import { ResetPasswordPageComponent } from './reset-password-page/reset-password-page.component';
