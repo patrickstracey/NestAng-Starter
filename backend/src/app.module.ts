feb5cd9f431e24092c75f54029baa80946009282
patrickstracey <pstracey17@gmail.com>
2023-05-29 13:21:35 -0700

    fix imports

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,10 +3,9 @@ import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard, PermissionGuard } from './utility/guards';
 import { UserModule } from './routes/user';
 import { AuthModule } from './routes/auth';
-import { MailModule } from './mail';
 
 @Module({
-  imports: [AuthModule, MailModule, UserModule],
+  imports: [AuthModule, UserModule],
   controllers: [],
   providers: [
     { provide: APP_GUARD, useClass: JwtAuthGuard },
--- backend/src/routes/user/user.module.ts
+++ backend/src/routes/user/user.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { MongoModule } from '../../database/mongo';
+import { MailModule } from '../../mail';
 
 @Module({
-  imports: [MongoModule],
+  imports: [MongoModule, MailModule],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
