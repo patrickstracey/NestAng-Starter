f701ae3aca54dcc19f071fdafcf83fa7003d85c1
Patrick Stracey <pstracey17@gmail.com>
2023-05-29 13:45:20 -0700

    Initial Mail Module (#5)
    * install `@sendgrid/mail`
    
    * init mail module
    
    * update with mail module info
    
    * send welcome email on signup
    
    * fix imports
    
    * put email domain in env

--- backend/environments/environment.ts
+++ backend/environments/environment.ts
@@ -7,4 +7,9 @@ export const environment = {
     mongo_uri: 'mongodb://localhost:27017', //DO NOT EXPOSE THIS VALUE IN A PROD ENVIRONMENT!
     name: 'nestAng',
   },
+  sendgrid: {
+    api_key: '{{YOUR_SENDGRID_API_KEY}}',
+    url: 'http://localhost:4200',
+    email_domain: '@{{YOUR_EMAIL_DOMAIN}}.com',
+  },
 };
