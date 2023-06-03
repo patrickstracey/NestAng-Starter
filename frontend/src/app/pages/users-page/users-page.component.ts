b76a2c21ba74a56fc3cfb2776d45dff492c3d192
patrickstracey <pstracey17@gmail.com>
2023-06-03 10:28:01 -0700

    initial commit of users page and module

---
+++ frontend/src/app/pages/users-page/users-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'page-users',
+  templateUrl: './users-page.component.html',
+  styleUrls: ['./users-page.component.scss'],
+})
+export class UsersPageComponent {}
