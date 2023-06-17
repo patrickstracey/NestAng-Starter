96a3b4c6564292faad46ef26edbae4018bc1f418
patrickstracey <pstracey17@gmail.com>
2023-06-17 13:42:26 -0700

    image viewer component

---
+++ frontend/src/app/modules/uploads/image-viewer/image-viewer.component.ts
@@ -0,0 +1,38 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+
+@Component({
+  selector: 'image-viewer',
+  templateUrl: './image-viewer.component.html',
+  styleUrls: ['./image-viewer.component.scss'],
+})
+export class ImageViewerComponent implements OnInit {
+  imageUrl = environment.IMAGE_URL;
+  @Input() images: string[] = [];
+  currentImage: string | null = null;
+  imageIndex: number = 0;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  viewImage(index: number) {
+    this.imageIndex = index;
+    this.currentImage = this.images[index];
+  }
+
+  closeOverlay() {
+    this.currentImage = null;
+  }
+
+  cycleImage(newIndex: number) {
+    let nextImage: number = newIndex;
+    if (newIndex < 0) {
+      nextImage = this.images.length - 1;
+    } else if (newIndex >= this.images.length) {
+      nextImage = 0;
+    }
+    this.imageIndex = nextImage;
+    this.currentImage = this.images[nextImage];
+  }
+}
