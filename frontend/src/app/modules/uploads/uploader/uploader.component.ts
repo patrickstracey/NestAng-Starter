2d8462d6b18983a07e151297a068babe164d639b
patrickstracey <pstracey17@gmail.com>
2023-06-17 17:48:05 -0700

    move upload logic into component

--- frontend/src/app/modules/uploads/uploader/uploader.component.ts
+++ frontend/src/app/modules/uploads/uploader/uploader.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
+import { BaseInterface } from '../../../../../../shared/interfaces';
+import { UploadsService } from '../../../services';
+import { FormControl, Validators } from '@angular/forms';
 
 interface UploaderConfigInterface {
   string: 'image' | 'file';
@@ -25,7 +28,8 @@ const fileConfigs: UploaderConfigInterface = {
  * This component takes and displays files and emits them as form data, you should call the uploads service from the component
  * that is creating the entity these will be attached to. This allows you to subscribe to a creation event and then attempt
  * to attach the files to the newly created entity via the upload service while creating the impression of a single unified
- * step for your users.
+ * step for your users. If your entity already exists, you can pass `attached_to` as an `Input()` and have this component
+ * handle all of the needed upload logic for that entity within the component.
  */
 @Component({
   selector: 'uploader',
@@ -37,19 +41,27 @@ export class UploaderComponent implements OnInit {
   previews: any[] = [];
   fileNames: string[] = [];
   fileErrors: string[] = [];
-  uploadedImages = new FormData();
+  uploadedItems = new FormData();
   readableTypeString: string = 'images';
   configs: UploaderConfigInterface = imageConfigs;
+  saving: boolean = false;
+  documentDisplayName: FormControl = new FormControl<string | null>(
+    null,
+    Validators.required
+  );
+
+  @Input() limit: number = 15;
 
   @Input() uploaderType: 'file' | 'image' = 'image';
-  @Input() limit: number = 15;
-  @Output() imagesUpdated = new EventEmitter<FormData>();
+  @Input() attached_to?: BaseInterface;
+  @Output() uploadItemsUpdated = new EventEmitter<FormData>();
 
-  constructor() {}
+  constructor(private _uploads: UploadsService) {}
 
   ngOnInit(): void {
     if (this.uploaderType == 'file') {
       this.configs = fileConfigs;
+      this.limit = 1;
     }
 
     if (this.limit > 1) {
@@ -65,7 +77,7 @@ export class UploaderComponent implements OnInit {
       for (let i = 0; i < this.selectedFiles.length; i++) {
         if (this.selectedFiles[i].size < this.configs.sizeLimit) {
           //3MB (3000000) - avg iphone image
-          this.uploadedImages.append(
+          this.uploadedItems.append(
             this.configs.string,
             this.selectedFiles[i],
             this.selectedFiles[i].name
@@ -88,14 +100,55 @@ export class UploaderComponent implements OnInit {
   }
 
   removeImages() {
-    this.uploadedImages = new FormData();
+    this.uploadedItems = new FormData();
     this.previews = [];
     this.fileNames = [];
     this.fileErrors = [];
+    this.documentDisplayName.reset();
     this.emitFormData();
   }
 
   emitFormData() {
-    this.imagesUpdated.emit(this.uploadedImages);
+    this.uploadItemsUpdated.emit(this.uploadedItems);
+  }
+
+  upload() {
+    if (this.attached_to && this.selectedFiles) {
+      this.saving = true;
+      if (this.uploaderType === 'image') {
+        this._uploads
+          .uploadImages(this.attached_to, this.uploadedItems)
+          .subscribe({
+            next: () => {
+              this.saving = false;
+              this.removeImages();
+            },
+            error: () => {
+              this.saving = false;
+            },
+          });
+      } else if (
+        this.uploaderType === 'file' &&
+        this.documentDisplayName.valid
+      ) {
+        this.uploadedItems.append(
+          'displayName',
+          this.documentDisplayName.value!
+        );
+        this._uploads
+          .uploadDocument(this.attached_to, this.uploadedItems)
+          .subscribe({
+            next: () => {
+              this.saving = false;
+              this.removeImages();
+            },
+            error: () => {
+              this.saving = false;
+            },
+          });
+      } else {
+        this.documentDisplayName.markAsTouched();
+      }
+    }
   }
 }
