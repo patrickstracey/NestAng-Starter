e90f06a20bbafd2c835bab1005ea70229de2035a
patrickstracey <pstracey17@gmail.com>
2023-06-18 07:48:37 -0700

    hookup org component

--- frontend/src/app/pages/organization-page/organization-page.component.ts
+++ frontend/src/app/pages/organization-page/organization-page.component.ts
@@ -1,8 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OrganizationService } from '../../services';
+import { FormControl, Validators } from '@angular/forms';
+import { OrganizationInterface } from '../../../../../shared/interfaces';
 
 @Component({
   selector: 'page-organization',
   templateUrl: './organization-page.component.html',
   styleUrls: ['./organization-page.component.scss'],
 })
-export class OrganizationPageComponent {}
+export class OrganizationPageComponent implements OnInit {
+  nameEdit: boolean = false;
+  name = new FormControl<string | null>(null, Validators.required);
+  organization: OrganizationInterface | null = null;
+  loading: boolean = true;
+
+  constructor(private _org: OrganizationService) {}
+
+  ngOnInit() {
+    this.loadOrganization();
+  }
+
+  async loadOrganization() {
+    this._org.getOrganization().subscribe({
+      next: (res) => {
+        this.organization = res;
+        this.name.patchValue(res.name);
+        this.loading = false;
+      },
+      error: () => {
+        this.loading = false;
+      },
+    });
+  }
+
+  editName() {
+    if (this.nameEdit) {
+      this.nameEdit = false;
+      if (this.name.value && this.name.value != this.organization?.name) {
+        this._org.patchOrganization({ name: this.name.value! });
+      }
+    } else {
+      this.nameEdit = true;
+    }
+  }
+}
