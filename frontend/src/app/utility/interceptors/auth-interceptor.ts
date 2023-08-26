bd5aa27420f14994b11bdf2e74c3aed66600005f
patrickstracey <pstracey17@gmail.com>
2023-08-26 11:09:20 -0700

    undoing where I done goofed it

--- frontend/src/app/utility/interceptors/auth-interceptor.ts
+++ frontend/src/app/utility/interceptors/auth-interceptor.ts
@@ -14,35 +14,32 @@ export class AuthInterceptor implements HttpInterceptor {
   constructor(private authService: AuthService) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
-    try {
-      const standardHeaders = new HttpHeaders({
+    let reqHeader: HttpHeaders;
+    if (this.authService.authenticated$.value?.access_token) {
+      reqHeader = new HttpHeaders({
         Authorization: `Bearer ${this.authService.authenticated$.value?.access_token}`,
       });
-
-      const modifiedRequest = req.clone({
-        headers: standardHeaders,
-      });
-
-      return next.handle(modifiedRequest).pipe(
-        tap({
-          next: () => {},
-          error: (err) => {
-            if (err instanceof HttpErrorResponse) {
-              if (err.status == 401) {
-                this.authService.logout();
-              } else if (err.status == 403) {
-                //TO DO: forbidden error handling
-              }
-            }
-          },
-        })
-      );
-    } catch (e) {
-      this.authService.logout();
-      throw new HttpErrorResponse({
-        status: 401,
-        error: 'You must be logged in to do that',
-      });
+    } else {
+      reqHeader = new HttpHeaders();
     }
+
+    const modifiedRequest = req.clone({
+      headers: reqHeader,
+    });
+
+    return next.handle(modifiedRequest).pipe(
+      tap({
+        next: () => {},
+        error: (err) => {
+          if (err instanceof HttpErrorResponse) {
+            if (err.status == 401) {
+              this.authService.logout();
+            } else if (err.status == 403) {
+              //TO DO: forbidden error handling
+            }
+          }
+        },
+      })
+    );
   }
 }
