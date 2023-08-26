d59f820be59609ce50181a9e14e7dbcc4d962be7
patrickstracey <pstracey17@gmail.com>
2023-08-26 09:12:56 -0700

    listen to auth Behaviorsubject to determine login and admin status

--- frontend/src/app/modules/navigation/nav-bar/nav-bar.component.ts
+++ frontend/src/app/modules/navigation/nav-bar/nav-bar.component.ts
@@ -1,34 +1,21 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Component, inject } from '@angular/core';
+import { map, Observable } from 'rxjs';
 import { AuthService } from '../../../services';
+import { PermissionEnum } from '../../../../../../shared/enums';
 
 @Component({
   selector: 'nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss'],
 })
-export class NavBarComponent implements OnInit, OnDestroy {
-  authSub!: Subscription;
-  isAdmin: boolean = false;
+export class NavBarComponent {
+  private authService = inject(AuthService);
 
-  constructor(private authService: AuthService) {}
-
-  ngOnInit() {
-    this.subscribeToAuth();
-  }
-
-  ngOnDestroy() {
-    this.isAdmin = false;
-    this.authSub.unsubscribe();
-  }
+  isAdmin$: Observable<boolean> = this.authService.authenticated$?.pipe(
+    map((session) => session?.permission == PermissionEnum.ADMIN)
+  );
 
   logout() {
     this.authService.logout();
-  }
-
-  subscribeToAuth() {
-    this.authSub = this.authService.authenticated$.subscribe((authValue) => {
-      this.isAdmin = authValue.admin;
-    });
   }
 }
