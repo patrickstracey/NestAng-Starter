5438e7739202bc4a5f174a96f978783ec0b69e32
patrickstracey <pstracey17@gmail.com>
2023-08-27 15:31:20 -0700

    move org pages to declarative BehaviorSubjects

--- frontend/src/app/pages/organization-page/organization-page.component.ts
+++ frontend/src/app/pages/organization-page/organization-page.component.ts
@@ -1,45 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { OrganizationService } from '../../services';
 import { FormControl, Validators } from '@angular/forms';
-import { OrganizationInterface } from '../../../../../shared/interfaces';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'page-organization',
   templateUrl: './organization-page.component.html',
   styleUrls: ['./organization-page.component.scss'],
 })
-export class OrganizationPageComponent implements OnInit {
+export class OrganizationPageComponent {
   nameEdit: boolean = false;
   name = new FormControl<string | null>(null, Validators.required);
-  organization: OrganizationInterface | null = null;
-  loading: boolean = true;
+  organization$ = this.orgService
+    .getOrganization()
+    .pipe(tap((res) => this.name.patchValue(res!.name)));
 
   constructor(private orgService: OrganizationService) {}
-
-  ngOnInit() {
-    this.loadOrganization();
-  }
-
-  async loadOrganization() {
-    this.orgService.getOrganization().subscribe({
-      next: (res) => {
-        this.organization = res;
-        this.name.patchValue(res.name);
-        this.loading = false;
-      },
-      error: () => {
-        this.loading = false;
-      },
-    });
-  }
 
   editName() {
     if (this.nameEdit) {
       this.nameEdit = false;
-      if (this.name.value && this.name.value != this.organization?.name) {
-        this.orgService
-          .patchOrganization({ name: this.name.value! })
-          .subscribe((res) => (this.organization = res));
+      if (this.name.value?.trim() != '') {
+        this.orgService.patchOrganization({ name: this.name.value! });
       }
     } else {
       this.nameEdit = true;
--- frontend/src/app/services/organization.service.ts
+++ frontend/src/app/services/organization.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { first, Observable, of, tap } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { OrganizationInterface } from '../../../../shared/interfaces';
 
 @Injectable({
@@ -8,42 +8,28 @@ import { OrganizationInterface } from '../../../../shared/interfaces';
 })
 export class OrganizationService {
   private baseUrl = 'api/organization';
-  private organization: OrganizationInterface | null = null;
+  private orgSubject = new BehaviorSubject<OrganizationInterface | null>(null);
 
   constructor(private http: HttpClient) {}
 
-  private fetchOrganization(): Observable<OrganizationInterface> {
-    return this.http.get<OrganizationInterface>(this.baseUrl).pipe(
-      tap((org) => {
-        this.setupOrganization(org);
-      })
-    );
+  private fetchOrganization() {
+    this.http
+      .get<OrganizationInterface>(this.baseUrl)
+      .subscribe((org) => this.orgSubject.next(org));
   }
 
-  setupOrganization(org: OrganizationInterface) {
-    this.organization = org;
-  }
-
-  getOrganization(): Observable<OrganizationInterface> {
-    if (this.organization) {
-      return of(this.organization).pipe(first());
+  getOrganization(): Observable<OrganizationInterface | null> {
+    if (this.orgSubject.value == null) {
+      this.fetchOrganization();
     }
-    return this.fetchOrganization();
+    return this.orgSubject;
   }
 
-  patchOrganization(orgChanges: {
-    name: string;
-  }): Observable<OrganizationInterface> {
-    return this.http
-      .patch<OrganizationInterface>(this.baseUrl, orgChanges)
-      .pipe(
-        tap((res) => {
-          this.setupOrganization(res);
-        })
-      );
-  }
-
-  resetService() {
-    this.organization = null;
+  patchOrganization(orgChanges: { name: string }) {
+    if (orgChanges.name != this.orgSubject.value?.name) {
+      this.http
+        .patch<OrganizationInterface>(this.baseUrl, orgChanges)
+        .subscribe((update) => this.orgSubject.next(update));
+    }
   }
 }
