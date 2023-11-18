2a7306d3c2f26faf7fa9f9b6c702f14a735930b0
patrickstracey <pstracey17@gmail.com>
2023-11-17 21:25:21 -0800

    move navbars to standalone

--- frontend/src/app/modules/navigation/nav-bar/nav-bar.component.ts
+++ frontend/src/app/modules/navigation/nav-bar/nav-bar.component.ts
@@ -1,13 +1,18 @@
 import { Component, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { map, Observable } from 'rxjs';
 import { AuthService } from '../../../services';
 import { PermissionEnum } from '../../../../../../shared/enums';
+import { NavBarMobileComponent } from '../nav-bar-mobile/nav-bar-mobile.component';
 import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss'],
+  standalone: true,
+  imports: [RouterModule, CommonModule],
 })
 export class NavBarComponent {
   private authService = inject(AuthService);
