f04e50d07fd32a8363bc528fcf93cd1e498af0a2
Marcel <luz.marcel@ymail.com>
2023-12-23 12:09:25 +0100

    integrate angular material

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -8,11 +8,13 @@ import { provideRouter } from '@angular/router';
 import { AuthInterceptor } from '@utility/interceptors';
 import { APP_ROUTES } from './app/app-routes';
 import { AppComponent } from './app/app.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(APP_ROUTES),
     provideHttpClient(withInterceptorsFromDi()),
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
-  ],
+    provideAnimations()
+],
 });
