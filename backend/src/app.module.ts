93f701cc757c328b7c05f454bf81fc9576cc3722
Marcel <luz.marcel@ymail.com>
2023-12-23 14:35:02 +0100

    backend serves frontend for docker usage

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,14 +4,21 @@ import { JwtAuthGuard, PermissionGuard } from './utility/guards';
 import { DatabaseModule } from './database';
 import { UserModule } from './routes/user';
 import { AuthModule } from './routes/auth';
-import {RiddleModule} from './routes/riddle'
+import { RiddleModule } from './routes/riddle'
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [DatabaseModule, AuthModule, UserModule, RiddleModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    DatabaseModule, AuthModule, UserModule, RiddleModule,
+  ],
   controllers: [],
   providers: [
     { provide: APP_GUARD, useClass: JwtAuthGuard },
     { provide: APP_GUARD, useClass: PermissionGuard },
   ],
 })
-export class AppModule {}
+export class AppModule { }
