9e59258617d1d5159a5ba1be61e539b35e3f2a1e
SaschaNenDatum <sascha.drobig@yahoo.de>
2023-12-23 14:19:18 +0100

    podcastjson

--- initial_data/auto_import.sh
+++ initial_data/auto_import.sh
@@ -1,1 +1,2 @@
 mongoimport --file=docker-entrypoint-initdb.d/users.json --jsonArray -d nestAng -c users --drop
+mongoimport --file=docker-entrypoint-initdb.d/podcast.json --jsonArray -d nestAng -c users --drop
