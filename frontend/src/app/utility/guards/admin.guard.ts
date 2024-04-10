c6078bf8f57fa24b9c66059213e82935901b7dd1
Patrick <pstracey17@gmail.com>
2024-04-09 22:09:00 -0700

    shorten import

--- frontend/src/app/utility/guards/admin.guard.ts
+++ frontend/src/app/utility/guards/admin.guard.ts
@@ -1,6 +1,6 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
-import { AuthService } from '../../services';
+import { AuthService } from '@services';
 
 export const AdminGuardFn: CanActivateFn = () => {
   if (inject(AuthService).isAdmin()) {
